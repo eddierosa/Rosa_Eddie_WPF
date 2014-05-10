204c6407fd286ca0a6bb89f2bbdb5309e0e93b48
eddierosa <earosa90@fullsail.edu>
2014-05-09 21:40:08 -0500

    Expressions

---
+++ Expressions/js/script.js
@@ -0,0 +1,35 @@
+//Expressions
+
+var a = 2; // Sets up our variable a and defines it with 2
+var b;
+b = a + 3; //add 3 to a
+
+console.log(b);
+
+
+//Expressions - Arithemetic Operators
+
+//area of the triangle is half of the width times the height
+
+var width = 4;
+var height = 5;
+var area = width * height/2;
+
+console.log(area);
+
+// PEMDAS - Order of Operations
+
+var quiz1 = 87;
+var quiz2 = 100;
+var quiz3 = 60;
+var quiz4 = 80;
+
+// The average is the sum of the items divided by the number of items
+
+var average = (quiz1 + quiz2 + quiz3 + quiz4) / 4;
+console.log(average);
+
+var length = 7;
+var width = 6;
+var perimeter = length * 2 + width * 2;
+console.log(perimeter);
