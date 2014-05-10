295e2eb15859a0f408a206882782787e1c1290a9
eddierosa <earosa90@fullsail.edu>
2014-05-09 20:58:05 -0500

    Variables

---
+++ Variables/js/script.js
@@ -0,0 +1,18 @@
+var yearBorn = 1990;
+
+console.log(yearBorn);
+
+alert(yearBorn);
+
+var age; //declaration
+age = 23; // definition
+
+var yearOfBirth = 1990; // declaring and defining on the same line
+
+var isStudent = false; // Boolean variable
+
+var firstName = "Eddie";
+
+var phrase = 'I don\'t know \n You\'ll have to speak with the professor'';
+
+
