57204b3c71ec45cfe0bab394f006b26f610edff8
eddierosa <earosa90@fullsail.edu>
2014-05-11 10:52:32 -0500

    Expressions_worksheet

--- Expressions_worksheet/Expressions Worksheet/js/script.js
+++ Expressions_worksheet/Expressions Worksheet/js/script.js
@@ -15,9 +15,10 @@ console.log(result); //The result of Sparky's age
 
 //Slice of Pie Part 1
 
-var slicesOfPizza = 8; //Slices of pizza per pie
+var slicesOfPizza = 4; //Slices of pizza per pie
 var numberOfPeople = 10; //Number of people at party
-var numberOrdered = 4; //Amount of pies ordered
+var numberOrdered = 8; //Amount of pies ordered
+var totalSlices = slicesOfPizza * numberOrdered; //Total amount of slices
 
 var amountEaten = (slicesOfPizza / numberOfPeople) *  numberOrdered; //Calculation used to determine the amount of slices per person
 
@@ -29,9 +30,11 @@ console.log(result2);
 
 
 
-var sparkysSlices =   ;
+var sparkysSlices = totalSlices % numberOfPeople; //Equation used to get the remainder of pizza slices
 
-console.log(sparkysSlices);
+var result3 = "Sparky got " + " " + (sparkysSlices) + " " + "slices of pizza.";
+
+console.log(result3);
 
 
 
