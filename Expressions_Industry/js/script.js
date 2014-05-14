d07d6595c0dd9f6c198bac0373e940b1be7d909c
eddierosa <earosa90@fullsail.edu>
2014-05-14 14:28:32 -0500

    Expressions_Industry

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,14 +1,14 @@
-var amountOfSites = prompt("Enter the amount of client sites that needs coverage:")
+var amountOfSites = prompt("Enter the amount of client sites that needs coverage:") //user enters amount of client sites
 
-var hourSites = prompt("Enter total amount of hours for client sites that need to be covered for the week:");
+var hourSites = prompt("Enter total amount of hours for client sites that need to be covered for the week:"); //user enters total hours for sites
 
-var amountOfEmployees = prompt("Enter amount of employees available.");
+var amountOfEmployees = prompt("Enter amount of employees available."); //user enters employees that are available
 
-var employeeHours = amountOfEmployees * 40; //200
+var employeeHours = amountOfEmployees * 40; //total amount of hours employees are available
 
-var employeesNeeded = hourSites / amountOfEmployees; // 30
+var employeesNeeded = hourSites / amountOfEmployees; //amount of employees to cover without going over 40 hours
 
-var remainingHours = hourSites - employeeHours;
+var remainingHours = hourSites - employeeHours; //extra hours that will result in overtime 
 
 
 alert("To cover " + amountOfSites + " " + " the shifts with no employee going over 40 hours is " + employeesNeeded);
