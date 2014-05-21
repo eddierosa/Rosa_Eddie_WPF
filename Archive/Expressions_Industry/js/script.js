0bab15ff37bbf0dda4442ac3020c6ea71307db9e
eddierosa <earosa90@fullsail.edu>
2014-05-20 23:58:50 -0500

    Expressions

---
+++ Archive/Expressions_Industry/js/script.js
@@ -0,0 +1,17 @@
+var amountOfSites = prompt("Enter the amount of client sites that needs coverage:") //user enters amount of client sites
+
+var hourSites = prompt("Enter total amount of hours for client sites that need to be covered for the week:"); //user enters total hours for sites
+
+var amountOfEmployees = prompt("Enter amount of employees available."); //user enters employees that are available
+
+var employeeHours = amountOfEmployees * 40; //total amount of hours employees are available
+
+var employeesNeeded = hourSites / amountOfEmployees; //amount of employees to cover without going over 40 hours
+
+var remainingHours = hourSites - employeeHours; //extra hours that will result in overtime
+
+
+alert("To cover " + amountOfSites + " " + " the shifts with no employee going over 40 hours is " + employeesNeeded);
+
+alert("Amount of hours left over are " + remainingHours + " that need to be filled.");
+
---
+++ Archive/Expressions_Personal/js/script.js
@@ -0,0 +1,15 @@
+// Eddie Rosa
+//Expressions Personal
+//May 11, 2014
+
+alert("We will be calculating how much it will cost you to completely fill your gas tank from empty")
+
+var amountGas = prompt("Please enter the amount of gas your car can hold:");
+var perGallon = prompt("Please enter the price of gas per gallon:");
+
+var actualCost = perGallon * amountGas;
+
+prompt("The amount that you are paying for gas is $" + actualCost + " for " + amountGas + " gallons of gas that your car can hold." );
+
+
+
