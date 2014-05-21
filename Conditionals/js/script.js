86a21cfb5e473d5682c2693af682b31a3be1bb61
eddierosa <earosa90@fullsail.edu>
2014-05-21 00:10:52 -0500

    Conditionals

---
+++ Conditionals/js/script.js
@@ -0,0 +1,12 @@
+//Conditional Logic
+
+var oldEnough = false;
+
+//if child is old enough, print to the console "you can ride!"
+
+if(oldEnough){
+     //code performed if condition is true
+    console.log("You can ride the coaster!");
+
+}
+console.log("What comes after");
