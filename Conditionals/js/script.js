900f68fbcd0dbd74381aa164a88e8e595e9ce574
eddierosa <earosa90@fullsail.edu>
2014-05-21 00:14:29 -0500

    Conditionals

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -10,3 +10,15 @@ if(oldEnough){
 
 }
 console.log("What comes after");
+
+//Relational Expressions
+
+var kidHeight = 50;
+var minHeight = 48;
+//if the kid is over 48 inches in height
+if(kidHeight > minHeight){
+    //code performed if condition is true
+    console.log("You can ride the coaster!")
+
+}
+
