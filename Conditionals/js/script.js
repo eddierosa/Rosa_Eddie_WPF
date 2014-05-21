c995b3770b42471ea4322fcfafd0bd8841d3c3a4
eddierosa <earosa90@fullsail.edu>
2014-05-21 00:16:36 -0500

    Conditionals

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -14,9 +14,10 @@ console.log("What comes after");
 //Relational Expressions
 
 var kidHeight = 50;
-var minHeight = 48;
+var minHeight = 47;
+var sneakerLift = 2;
 //if the kid is over 48 inches in height
-if(kidHeight > minHeight){
+if(kidHeight + sneakerLift > minHeight){
     //code performed if condition is true
     console.log("You can ride the coaster!")
 
