e12f33764c90184d279534afae0777964b0d2f93
eddierosa <earosa90@fullsail.edu>
2014-05-21 00:44:38 -0500

    TernaryOperatos

--- Conditionals/js/script.js
+++ Conditionals/js/script.js
@@ -11,3 +11,19 @@ if( gpa > 2.0){
 
 (gpa > 2.0) ? console.log("You can graduate") : console.log("GPA too low");
 
+
+var age = 11;
+var book;
+
+//if the child is under 10, they get Green Eggs and Ham, otherwise they get The Time Machine
+if(age < 10){
+    book = "Green Eggs and Ham";
+}else
+{
+    book = "The Time Machine";
+}
+
+console.log(book)
+book = (age <10) ? "Green Eggs and Ham" : "The Time Machine";
+
+
