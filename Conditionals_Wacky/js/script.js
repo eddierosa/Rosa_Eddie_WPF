5d4ada24dc44e24616bed1c6ac2af33a8e27f12b
eddierosa <earosa90@fullsail.edu>
2014-05-22 19:49:55 -0500

    Conditionals_Wacky

---
+++ Conditionals_Wacky/js/script.js
@@ -0,0 +1,23 @@
+var again;
+
+
+if(again == 'yes'){
+    var toyStoryChar = prompt("Please enter either Woody, Buzz Lighter or Mr. Potato Head");
+    
+    if(toyStoryChar = 'Woody'){
+        alert("Round Them Up CowBoy!");
+
+    }
+    else if(toyStoryChar = 'Buzz Lighter'){
+        alert("To Infinite And Beyond!");
+    }
+    else if(toyStoryChar = 'Mr. Potato Head'){
+        alert("Where's My Eye?!");
+    }
+
+     prompt("Would you like to enter another Toy Story character? Please type Yes or No");
+
+    else{
+        alert("Thanks for your time!");
+    }
+}
